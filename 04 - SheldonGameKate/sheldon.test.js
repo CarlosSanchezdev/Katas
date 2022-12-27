51e06617d509852436d750ec206576a7e0968d56
Carlos Sánchez <holapueblodev@gmail.com>
2022-12-27 13:59:53 +0100

    first commit kata Sheldon

---
+++ 04 - SheldonGameKate/sheldon.test.js
@@ -0,0 +1,15 @@
+test('introducimos 3 seria igual a FIZZ', () => {
+    expect(hola(3)).toBe('FIZZ');
+});
+
+test('introducimos 3 seria igual a FIZZ', () => {
+    expect(hola(5)).toBe('BUZZ');
+});
+
+test('introducimos 3 seria igual a FIZZ', () => {
+    expect(hola(15)).toBe('FIZZBUZZ');
+});
+
+test('introducimos 3 seria igual a FIZZ', () => {
+    expect(hola(4)).toBe(4);
+});
